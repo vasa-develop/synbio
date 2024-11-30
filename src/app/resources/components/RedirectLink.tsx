e8aa9010ddc6d47905209e5c2eb3bcdb4b3b8e10
vasa <vasa.develop@gmail.com>
2024-11-30 17:19:29 +0400

    Update RedirectLink.tsx

--- src/app/resources/components/RedirectLink.tsx
+++ src/app/resources/components/RedirectLink.tsx
@@ -6,13 +6,7 @@ import React from 'react'
 export default function RedirectLink({link}:{link:string}) {
    const router = useRouter();
 
-  useEffect(() => {
-    // Redirect to GitHub repository after a slight delay for the Twitter crawler
-    setTimeout(() => {
-      // Using router.replace to avoid pushing a new entry into the history stack
-      router.replace(link);
-    }, 3000);  // 3000 milliseconds delay
-  }, [link,router]);
+  useEffect(() => {}, [link,router]);
    
   return (
     <div>RedirectLink</div>
