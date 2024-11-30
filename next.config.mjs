d01fde3977f4bd1b4ba644c19c1fd653d69dd7fb
vasa <vasa.develop@gmail.com>
2024-11-30 17:25:49 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['synbio.sh'],
+    domains: ['synbio.sh'], // Existing configuration
+  },
+  async redirects() {
+    return [
+      {
+        source: '/resources', // Path to redirect from
+        destination: 'https://vasa.bio/publications-talks/how-to-learn-synthetic-biology-as-a-programmer', // External URL to redirect to
+        permanent: true, // Use true for a 308 permanent redirect
+      },
+    ];
   },
 };
 
